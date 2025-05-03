77c8471ee4c73be0c656355f2810137c5cbfe4cd
takasugikensuke <k.takasugi.biz@gmail.com>
2025-05-04 07:30:58 +0900

    feat: 多言語対応サンプルアプリの初期実装

---
+++ lib/common/bottom_navigation.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:sample_l10n_key_preview/sample_a/sample_a_screen.dart';
+import 'package:sample_l10n_key_preview/sample_b/sample_b_screen.dart';
+import 'package:sample_l10n_key_preview/sample_c/sample_c_screen.dart';
+import 'package:sample_l10n_key_preview/sample_d/sample_d_screen.dart';
+import 'package:sample_l10n_key_preview/sample_e/sample_e_screen.dart';
+
+class BottomNavigation extends StatefulWidget {
+  const BottomNavigation({super.key});
+
+  @override
+  State<BottomNavigation> createState() => _BottomNavigationState();
+}
+
+class _BottomNavigationState extends State<BottomNavigation> {
+  int _selectedIndex = 0;
+
+  static const List<Widget> _widgetOptions = <Widget>[
+    SampleAScreen(),
+    SampleBScreen(),
+    SampleCScreen(),
+    SampleDScreen(),
+    SampleEScreen(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _widgetOptions.elementAt(_selectedIndex),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'A'),
+          BottomNavigationBarItem(icon: Icon(Icons.business), label: 'B'),
+          BottomNavigationBarItem(icon: Icon(Icons.school), label: 'C'),
+          BottomNavigationBarItem(icon: Icon(Icons.settings), label: 'D'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'E'),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
---
+++ lib/sample_d/sample_d_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:sample_l10n_key_preview/common/localized_text_group.dart';
+
+class SampleDScreen extends StatelessWidget {
+  const SampleDScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final l10n = AppLocalizations.of(context)!;
+
+    return Center(
+      child: LocalizedTextGroup(
+        texts: [l10n.thankYou, l10n.imSorry, l10n.please],
+      ),
+    );
+  }
+}
